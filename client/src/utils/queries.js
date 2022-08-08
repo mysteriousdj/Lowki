61962b948da1e827b7daa030f58e39456495596d
jesus villegas <jesusvicajr@hotmail.com>
2022-08-08 12:56:58 -0700

    ProfilePic

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,6 +6,7 @@ export const QUERY_USER = gql`
       _id
       username
       email
+      profilePic
       rants {
         _id
         rantText
@@ -49,6 +50,7 @@ export const QUERY_ME = gql`
       _id
       username
       email
+      profilePic
       rants {
         _id
         rantText
